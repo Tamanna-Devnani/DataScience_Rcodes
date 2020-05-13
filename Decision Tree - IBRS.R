8e41f52073bb1ff1dd010a6eb2b96fae0235f544
Tamanna-Devnani <64688918+Tamanna-Devnani@users.noreply.github.com>
2020-05-13 20:44:39 +0400

    Decision Tree and Prediction

---
+++ Decision Tree - IBRS.R
@@ -0,0 +1,18 @@
+data("iris")
+install.packages("caret")
+install.packages("C50")
+library(caret)
+library(C50)
+set.seed(7)
+inTraininglocal<-createDataPartition(iris$Species,p=.70,list = F)
+training<-iris[inTraininglocal,]
+testing<-iris[-inTraininglocal,]
+#Model Building
+model<-C5.0(Species~.,data = training) 
+#Generate the model summary
+summary(model)
+#Predict for test data set
+pred<-predict.C5.0(model,testing[,-5])
+a<-table(testing$Species,pred)
+sum(diag(a))/sum(a)
+plot(model)
